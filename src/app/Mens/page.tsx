2a9eb280aa599e8cfd5878d991b15535058f6089
Owaisi123 <afshanaslam091@gmail.com>
2025-02-07 00:08:02 +0500

    first commit

---
+++ src/app/Mens/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function mens() {
+  return (
+    <div>mens</div>
+  )
+}
---
+++ src/app/Womans/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function womans() {
+  return (
+    <div>womans</div>
+  )
+}
